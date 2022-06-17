49884565e2d870dadfda63e5b10b929cade96ec7
MohitGupta121 <guptamm.22@gmail.com>
2022-06-17 13:53:35 +0530

    project created'

---
+++ app/src/main/java/com/mohit/newsdo/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mohit.newsdo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mohit.newsdo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
