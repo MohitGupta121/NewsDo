3f573d5714e0ce4f4c8e01a2f78fc4adc1bed076
Mohit Gupta <guptamm.22@gmail.com>
2022-08-14 21:29:13 +0530

    fix saved news

--- app/src/main/java/com/mohit/newsdo/database/Converters.kt
+++ app/src/main/java/com/mohit/newsdo/database/Converters.kt
@@ -6,8 +6,8 @@ import com.mohit.newsdo.model.Source
 class Converters {
 
     @TypeConverter
-    fun fromSource(s:Source):String = s.name
+    fun fromSource(s: Source):String = s.name
 
     @TypeConverter
-    fun toSource(name:String):Source = Source(name,name)
+    fun toSource(name:String): Source = Source(name,name)
 }
